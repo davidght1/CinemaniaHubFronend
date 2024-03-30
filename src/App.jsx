5b50a2daf90a2547b9135f5ec51feaca1115b7e1
David Agam <davidght1@gmail.com>
2024-03-30 21:25:09 +0300

    modified:   src/App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Login from './pages/Login/Login'
-import Register from './pages/Register/Register'
-
-//context
-import { MovieProvider } from './context/MovieContext'
-import Navbar from './components/navbar/Navbar'
+// App.js
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import { MovieProvider } from './context/MovieContext';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
-  const isAuthenticated = false; // need implements later if user logged in or not
-  
+  const isAuthenticated = false; // Implement authentication logic here
+
   return (
     <MovieProvider>
       <BrowserRouter>
-      <Navbar isAuthenticated={isAuthenticated}/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
-        </Routes>
+        <Navbar isAuthenticated={isAuthenticated} />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </MovieProvider>
-  )
+  );
 }
 
-export default App
+export default App;
